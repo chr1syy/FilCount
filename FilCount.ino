e6390d8748ac7693c432475c8ad5d8cff03c1a7d
chr1syy <ch.bleuel@me.com>
2020-02-02 08:44:31 +0100

    Code changes
    Andere Library für RotaryEncoder, funktioniert jetzt mit Interrupts

--- FilCount.ino
+++ FilCount.ino
@@ -1,12 +1,10 @@
 // Libraries
-// Encoder funktioniert nur ohne Interrupt -> eventuell andere Library / ICACHE_RAM_ATTR in der Library einfügen
 
-#define ENCODER_DO_NOT_USE_INTERRUPTS
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Encoder.h>
+#include <RotaryEncoder.h>
 
 
 // OLED I²C Display
@@ -26,10 +24,11 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 // DT - D6
 // SW - D7
 // + - 3.3
-// GND - wohin wohl
-Encoder myEnc(D5, D6);
+// GND - G
+RotaryEncoder myEnc(D5, D6, D7);
 
-int oldPos = -999;
+int oldPos = 0;
+int newPos = 0;
 boolean isButtonPressed = false;
 long lastUpdateMillis = 0;
 
@@ -39,37 +38,53 @@ void ICACHE_RAM_ATTR handleKey() {
   isButtonPressed = true;  
 }
 
+void ICACHE_RAM_ATTR handleRot() {
+  myEnc.readAB();
+}
+
+// Variable
+
+String names[10];
+long len[10];
+
+
 void setup() {
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  myEnc.begin();
+  
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("Display-Test");
-  delay(100);
+  display.println("Filament-Counter");
+  display.println("Init");
   display.display();
 
-  // Interrupt auf Pin D7 für Button
+  delay(1000);
 
+  // Interrupt auf Pin D7 für Button, D5 & D6 für Rotary -> funktioniert schlechter als DO_NOT_USE_INTERRUPTS
+  attachInterrupt(digitalPinToInterrupt(D5), handleRot, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(D6), handleRot, CHANGE);
   attachInterrupt(digitalPinToInterrupt(D7), handleKey, RISING);
   
-  myEnc.write(0);
+  myEnc.setPosition(0);
 }
 
 void loop() {
   display.clearDisplay();
   display.setCursor(0, 0);
-  int newPos = myEnc.read();
+  newPos = myEnc.getPosition();
+  
   if (newPos != oldPos){
     oldPos = newPos;
   }
 
-  if (isButtonPressed && millis() - lastUpdateMillis > 50) {
+  if (isButtonPressed && millis() - lastUpdateMillis > 50) {  // debounce Rotary
     isButtonPressed = false;
     lastUpdateMillis = millis();
     // Reset the counter
-    myEnc.write(0);
+    myEnc.setPosition(0);
   }
   
   display.println(oldPos);
