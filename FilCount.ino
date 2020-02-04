57f52e50e571605fded510b9e0747804a640b36d
Kai Schillak <kschillak@gmx.de>
2020-02-04 01:16:55 +0100

    Multilanguage / Umlauts
    Added multi language support, UTF-8 support, a ton of fonts
    
    display.setCursor() is now lower left position!

--- FilCount.ino
+++ FilCount.ino
@@ -4,7 +4,11 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <u8g2_fonts.h>
+#include <U8g2_for_Adafruit_GFX.h>
 #include <RotaryEncoder.h>
+#include <ESP8266WiFi.h>
+#include "languages.h"
 
 
 // OLED I²C Display
@@ -12,12 +16,11 @@
 // SDA - D2
 // VCC - 3.3
 // GND - G
-
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    0
-
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+U8G2_FOR_ADAFRUIT_GFX u8g2;
 
 // Encoder
 // CLK - D5
@@ -45,20 +48,22 @@ void ICACHE_RAM_ATTR handleRot() {
 // Variable
 
 String names[10];
+char pos[5];
 long len[10];
 
 
 void setup() {
-  
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  u8g2.begin(display);
   myEnc.begin();
   
   display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println("Filament-Counter");
-  display.println("Init");
+  //display.setTextSize(1);
+  u8g2.setForegroundColor(WHITE);
+  u8g2.setFont(u8g2_font_unifont_tf); // 10 pixel font
+  u8g2.setCursor(0, 11);
+  u8g2.println(MSG_TITLE);
+  u8g2.println("Init");
   display.display();
 
   delay(1000);
@@ -73,7 +78,8 @@ void setup() {
 
 void loop() {
   display.clearDisplay();
-  display.setCursor(0, 0);
+  //display.setTextSize(2);  
+  u8g2.setCursor(30,20);
   newPos = myEnc.getPosition();
   
   if (newPos != oldPos){
@@ -86,8 +92,8 @@ void loop() {
     // Reset the counter
     myEnc.setPosition(0);
   }
-  
-  display.println(oldPos);
+  itoa(oldPos, pos, 10);
+  u8g2.print(oldPos);
   display.display();
 
 }
---
+++ languages.h
@@ -0,0 +1,9 @@
+//#define LANG_DE
+#ifdef LANG_DE
+  #define MSG_TITLE "Filament-Zähler"
+#endif
+
+#define LANG_EN
+#ifdef LANG_EN
+  #define MSG_TITLE "Filamant-Counter"
+#endif
