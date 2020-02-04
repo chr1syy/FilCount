60d5444d4175b1682a41aca6a4e22441f336e217
Kai Schillak <kschillak@gmx.de>
2020-02-04 02:12:53 +0100

    Added filament roll array

--- FilCount.ino
+++ FilCount.ino
@@ -10,6 +10,7 @@
 #include <ESP8266WiFi.h>
 #include "languages.h"
 
+#define NUMBER_OF_SPOOLS 50
 
 // OLED I²C Display
 // SCL - D1
@@ -30,13 +31,21 @@ U8G2_FOR_ADAFRUIT_GFX u8g2;
 // GND - G
 RotaryEncoder myEnc(D5, D6, D7);
 
+// Variables
+struct filamentRolls
+  {
+    int id;
+    String name;
+    float length;
+  };
+filamentRolls rolls[NUMBER_OF_SPOOLS];
+
 int oldPos = 0;
 int newPos = 0;
 boolean isButtonPressed = false;
 long lastUpdateMillis = 0;
 
 // Interrupt handler
-
 void ICACHE_RAM_ATTR handleKey() {
   isButtonPressed = true;  
 }
@@ -45,14 +54,13 @@ void ICACHE_RAM_ATTR handleRot() {
   myEnc.readAB();
 }
 
-// Variable
-
-String names[10];
-char pos[5];
-long len[10];
-
-
 void setup() {
+  // Beispiele für Rollen 
+  rolls[0].id = 1;
+  rolls[0].name = "PLA Rot";
+  rolls[0].length = 22.5;
+  rolls[1] = {1, "PETG Blau", 0};
+   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   u8g2.begin(display);
   myEnc.begin();
@@ -63,10 +71,10 @@ void setup() {
   u8g2.setFont(u8g2_font_unifont_tf); // 10 pixel font
   u8g2.setCursor(0, 11);
   u8g2.println(MSG_TITLE);
-  u8g2.println("Init");
+  u8g2.println(MSG_INIT);
   display.display();
 
-  delay(1000);
+  delay(3000);
 
   // Interrupt auf Pin D7 für Button, D5 & D6 für Rotary -> funktioniert schlechter als DO_NOT_USE_INTERRUPTS
   attachInterrupt(digitalPinToInterrupt(D5), handleRot, CHANGE);
@@ -78,8 +86,11 @@ void setup() {
 
 void loop() {
   display.clearDisplay();
-  //display.setTextSize(2);  
-  u8g2.setCursor(30,20);
+  u8g2.setCursor(0,11);
+  u8g2.print(rolls[1].name);  
+  u8g2.setCursor(55,40);
+
+  // Init Encoder mit pos. Muss man wohl umrechnen aus length.
   newPos = myEnc.getPosition();
   
   if (newPos != oldPos){
@@ -92,7 +103,6 @@ void loop() {
     // Reset the counter
     myEnc.setPosition(0);
   }
-  itoa(oldPos, pos, 10);
   u8g2.print(oldPos);
   display.display();
 
--- languages.h
+++ languages.h
@@ -1,9 +1,11 @@
-//#define LANG_DE
+#define LANG_DE
 #ifdef LANG_DE
   #define MSG_TITLE "Filament-Zähler"
+  #define MSG_INIT  "Initialisiere"
 #endif
 
-#define LANG_EN
+//#define LANG_EN
 #ifdef LANG_EN
   #define MSG_TITLE "Filamant-Counter"
+  #define MSG_INIT  "Init"
 #endif
