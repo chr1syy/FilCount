d1d6fa0e78c42ed3af1690b00a9019d5e2e93b08
chr1syy <ch.bleuel@me.com>
2020-02-04 17:45:37 +0100

    Update FilCount.ino

--- FilCount.ino
+++ FilCount.ino
@@ -36,13 +36,15 @@ struct filamentRolls
   {
     int id;
     String name;
-    float length;
+    int length;
   };
 filamentRolls rolls[NUMBER_OF_SPOOLS];
 
 int oldPos = 0;
 int newPos = 0;
+int selected = 0;
 boolean isButtonPressed = false;
+boolean isCounting = false;
 long lastUpdateMillis = 0;
 
 // Interrupt handler
@@ -58,9 +60,10 @@ void setup() {
   // Beispiele für Rollen 
   rolls[0].id = 1;
   rolls[0].name = "PLA Rot";
-  rolls[0].length = 22.5;
-  rolls[1] = {1, "PETG Blau", 0};
-   
+  rolls[0].length = 22;
+  rolls[1] = {2, "PETG Blau", 0};
+  rolls[2] = {3, "TPU Lila", 50};
+     
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   u8g2.begin(display);
   myEnc.begin();
@@ -85,25 +88,73 @@ void setup() {
 }
 
 void loop() {
+
+  // Falls im letzten Loop-Durchlauf der Zählmodus aktiviert wurde, springe in while-loop
+  while(isCounting){
+    display.clearDisplay();
+    u8g2.setCursor(0,11);
+    u8g2.print(rolls[selected].name);  
+    u8g2.setCursor(55,40);
+
+    // Werte zählen und anzeigen
+    newPos = myEnc.getPosition();
+    oldPos = newPos;
+
+    u8g2.print(oldPos);
+    display.display();
+
+    // Wenn auf den Knopf gedrückt wird
+    if (isButtonPressed && millis() - lastUpdateMillis > 50) {
+      isButtonPressed = false;
+      lastUpdateMillis = millis();
+
+
+      // Zählwert speichern und auf den alten Menüeintrag zurück springen
+      rolls[selected].length = oldPos;
+      myEnc.setPosition(selected);
+
+      isCounting = false;
+      break;
+    }
+  } 
+
+  // Merker für rolls[x]
+  selected = oldPos;
+  
   display.clearDisplay();
   u8g2.setCursor(0,11);
-  u8g2.print(rolls[1].name);  
+  u8g2.print(rolls[selected].name);  
   u8g2.setCursor(55,40);
 
-  // Init Encoder mit pos. Muss man wohl umrechnen aus length.
   newPos = myEnc.getPosition();
   
-  if (newPos != oldPos){
+  // Einstiegs-Menü, Auswahl der Rolle
+  // Unter ID 1 wollen wir nicht auswählen
+  if (newPos < 0) {
+    oldPos = 0;
+    myEnc.setPosition(0);
+  }
+  // Gibt es den nächsten Datensatz überhaupt? -> Neue Rolle erstellen / Einstellungen als Menüpunkt hier einfügen? 
+  else if (rolls[newPos].id == 0){
+    myEnc.setPosition(oldPos);
+  }
+  // Ansonsten weiter scrollen
+  else {
     oldPos = newPos;
   }
 
-  if (isButtonPressed && millis() - lastUpdateMillis > 50) {  // debounce Rotary
+  // Wenn der Knopf gedrückt wird die aktuelle Rolle auswählen
+  if (isButtonPressed && millis() - lastUpdateMillis > 50) {  
     isButtonPressed = false;
     lastUpdateMillis = millis();
-    // Reset the counter
-    myEnc.setPosition(0);
-  }
-  u8g2.print(oldPos);
+
+    isCounting = true;
+    // Encoder auf Zählwert stellen, ab dort dann weiter zählen
+    myEnc.setPosition(rolls[selected].length);
+ }
+
+  
+  u8g2.print(rolls[selected].length);
   display.display();
 
 }
